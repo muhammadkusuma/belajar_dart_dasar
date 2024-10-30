c5a40733cc02e42cc4628f214401de12834a506e
M. Wira Ade Kusuma <mwiraadekusuma1@gmail.com>
2024-10-31 00:14:37 +0700

    Komentar

---
+++ bin/komentar.dart
@@ -0,0 +1,14 @@
+/// ini ada adalah documentation
+/// yang biasanya akan digenerate menjadi dart doc
+
+void main() {
+  // this is variable
+  var name = "Wira";
+
+  /**
+   * Ini adalah komentar
+   * lebih dari satu baris
+   */
+
+  print(name);
+}
